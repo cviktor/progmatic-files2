5e2c57eef1f2a6642e4dc458e273375500b5bf9e
Czeglédi Viktor <czegledi.viktor@gmail.com>
2018-07-23 09:57:40 +0200

    Első fájl beolvasás

--- File/Program.cs
+++ File/Program.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +11,20 @@ namespace File
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //relatív elérési út az exe fájl helyétõl
+            //FileStream file = new FileStream(@"..\..\hello.txt", FileMode.Open);
+            //abszolút elérési út (egy meghajtó névvel kezdõdik és mindig ugyan oda mutat mindegy hol az exe)
+            FileStream file = new FileStream(@"F:\Projects\Progmatic\2018-07-23\File\File\hello.txt", FileMode.Open);
+
+
+            for (int i = 0; i < 5; i++)
+            {
+                int b = file.ReadByte();
+                Console.WriteLine(b);
+                Console.WriteLine((char)b);
+            }
+
+
             Console.ReadKey();
         }
     }
